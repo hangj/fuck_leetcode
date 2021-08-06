f145b43e1f822071a934c99f358c7b11d1944fb4
hangj <guijie.han@gmail.com>
2021-08-06 16:35:35 +0800

    「Manacher算法」备注

--- longestPalindrome.c
+++ longestPalindrome.c
@@ -39,6 +39,12 @@ char * longestPalindrome(char * s){
 
 
 
+// 官方还提供了一个「Manacher 算法」。整体思路就是，基于回文字符串的对称性，缓存前面字符的「臂长」信息，以便后面复用。
+// https://www.cnblogs.com/hangj/p/15109242.html 我在这篇博客对该算法做了更直观的解释
+// https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
+
+
+
 int main(int argc, char** argv, char** envp){
     if(argc != 2){
         printf("usage: %s string\n", argv[0]);
