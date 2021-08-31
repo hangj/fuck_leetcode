a681bd0a5e236edc39d7bda800a47ceaead230fa
hangj <guijie.han@gmail.com>
2021-08-31 16:57:56 +0800

    practice rust

--- two_sum.rs
+++ two_sum.rs
@@ -5,18 +5,17 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::new();
-        for i in 0 .. nums.len() {
-            let sub = target - nums[i];
-            if map.contains_key(&nums[i]) {
-                let j = map[&nums[i] ];
+        let mut map= HashMap::new();
+        for (i,v) in nums.iter().enumerate() {
+            let sub = target - v;
+            if map.contains_key(v) {
+                let j = map[v];
                 return vec![j as i32, i as i32];
             }else{
                 map.insert(sub, i);
-                // println!("sub:{},i:{}", sub,i);
             }
         }
-        return vec![0i32, 0i32];
+        return vec![0, 0];
     }
     
 }
