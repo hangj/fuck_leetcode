a78f50027c8224cb0ae278c14604005f4259459b
hangj <guijie.han@gmail.com>
2022-05-17 17:09:17 +0800

    learn rust by fucking leetcode

---
+++ length_of_longest_substring.rs
@@ -0,0 +1,30 @@
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        use std::collections::HashMap;
+        let mut hm = HashMap::<char, usize>::new();
+        let mut start = 0usize;
+        let mut stop = start;
+        let mut longest = 0usize;
+        for (i, c) in s.chars().enumerate(){
+            match hm.get(&c) {
+                Some(j) => {
+                    if *j >= start {
+                        start = j + 1;
+                    }
+                },
+                None => (),
+            }
+            hm.insert(c, i);
+            stop = i;
+
+            // println!("start: {}, stop: {}", start, stop);
+
+            // if start >= stop { return 0; }
+            if stop - start + 1 > longest {
+                longest = stop - start + 1;
+            }
+        }
+
+        longest as i32
+    }
+}
